0e89973731ef50adfd77c7778c2e3815cd350763
jamesbryer <james.bryer@ons.gov.uk>
2025-11-14 13:04:49 +0000

    Added upload method for bucket repository

--- sds_common/repositories/bucket_repository.py
+++ sds_common/repositories/bucket_repository.py
@@ -9,3 +9,7 @@ class BucketRepository:
 
     def get_file_as_json(self, filename: str):
         return json.loads(self.bucket.blob(filename).download_as_string())
+
+    def upload_file_from_path(self, filepath: str):
+        blob = self.bucket.blob(filepath)
+        blob.upload_from_filename(filepath)
