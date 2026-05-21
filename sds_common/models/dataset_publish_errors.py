c4945bc3a96b76398087539c772a63296840d4f7
jamesbryer <james.bryer@ons.gov.uk>
2026-05-21 09:25:38 +0100

    Add new models

---
+++ sds_common/models/dataset_publish_errors.py
@@ -0,0 +1,14 @@
+
+
+class DatasetPublishError(Exception):
+    pass
+
+class DatasetMetadataRetrievalError(DatasetPublishError):
+    def __init__(self, survey_id: str, period_id: str, status_code: int):
+        self.message = f"Failed to retrieve metadata for dataset with survey_id: {survey_id} and period_id: {period_id}, Status code: {status_code}"
+        super().__init__(self.message)
+
+class DatasetCreateError(DatasetPublishError):
+    def __init__(self, status_code: int):
+        self.message = f"Failed to call the dataset/create endpoint. Status code: {status_code}"
+        super().__init__(self.message)
